eab65e0663a03329f0291e01b7b4ea5e4a08dc10
lauriselvijs <lauriselvijsm@gmail.com>
2021-08-31 16:44:49 +0300

    edited server.js file

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.use("/api/v1/notes", notes);
 app.use("/api/v1/users", users);
 app.use("/api/v1/auth", auth);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
